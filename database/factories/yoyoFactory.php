4d58ea718b829bf68f82b061b0d9dbdb1a4e2f72
BICCOUNT-TECHNOLOGIES <github@biccounttechnologies.com>
2024-12-06 16:39:54 +0300

    DESIGN

---
+++ database/factories/yoyoFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class yoyoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'), // Default password
+            'remember_token' => Str::random(10),
+        ];
+    }
+}
